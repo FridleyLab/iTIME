1290428fc344b599bd4f20fbb774fabc61122957
jhcreed <jordan.h.creed@moffitt.org>
2020-12-04 11:10:35 -0500

    organizational folders

---
+++ R/hex-logo.R
@@ -0,0 +1,15 @@
+library(hexSticker)
+library(magick)
+
+# sticker ---------------------------------------------------------------------
+
+# <div>Icons made by <a href="https://www.flaticon.com/free-icon/zoom_900930" title="phatplus">phatplus</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+img <- image_read("figures/zoom.png")
+
+sticker(img, package="iTIME",
+        p_size=6, p_color = "#74bc1f", p_y = 1.1, p_x = 1.1,
+        s_x=1, s_y=1, s_width=1.4, s_height=1.25,
+        h_fill="white", h_color="#74bc1f", 
+        url = "https://github.com/FridleyLab/iTIME", u_color = "#74bc1f", u_size = 1.3,
+        filename="figures/hex.png")
