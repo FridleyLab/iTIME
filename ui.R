37fc88fa5e972ebc40d9b8e8ba3ed145204029f2
Markowitz <MarkowJ@moffitt.usf.edu>
2020-12-10 10:59:40 -0500

    testing

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Joseph"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
