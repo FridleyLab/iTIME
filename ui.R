6550ce96bab03ece3940d1094d741edc8f4fa7bf
Markowitz <MarkowJ@moffitt.usf.edu>
2020-12-10 11:10:10 -0500

    test

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Joseph"),
+    titlePanel("Joseph2"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
