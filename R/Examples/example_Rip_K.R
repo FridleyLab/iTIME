98f0febf5d888a0ac13601c6077d9f297b7b9cce
Alex Soupir <alex.soupir@sdstate.edu>
2020-12-11 11:38:11 -0500

    delete example

--- R/Examples/example_Rip_K.R
+++
@@ -1,6 +0,0 @@
-data = spatial[[2]] %>% select(XMin, YMin, CD3..CD8.) %>%
-  filter(CD3..CD8. == 1)
-w = convexhull.xy(data$XMin,data$YMin)
-point_proc = ppp(x = data$XMin, y = data$YMin, window=w) 
-K  = Kest(point_proc)
-plot(K)
