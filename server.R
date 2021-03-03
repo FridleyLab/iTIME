f336a165dc137c3d46ad69d63d77f3acae0de654
Alex Soupir <alex.soupir@jacks.sdstate.edu>
2021-03-02 22:11:21 -0500

    remove spatial global assignment for testing

--- server.R
+++ server.R
@@ -63,7 +63,6 @@ shinyServer(function(input, output) {
         } else {
             df = read.csv("data/Coghill_P2_Anal-Invasive-TMA1_[5,B].tif_74186_job45081.object_results copy.csv")
         }
-        assign('spatial', df, envir=globalenv())
         
         return(df)
     })
