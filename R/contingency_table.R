a7d55a4a93bb6f39b283a5184e7dc59e66bdb8b8
Alex Soupir <alex.soupir@jacks.sdstate.edu>
2021-03-17 21:38:07 -0400

    fixed incorrect summary, cleaned file

--- R/contingency_table.R
+++ R/contingency_table.R
@@ -1,21 +1,18 @@
-# library(epiDisplay)
-# library(tidyverse)
-# summary <- read_csv("data/summary.csv")
-# clinical <- read_csv("data/clinical.csv")
-
 # #Chris's function
 # #edited out % with markers to fix issues with contingency printout
+# added as.numeric for the percent threshold, don't know why such error
 
 contingency_table <- function(summary_clinical_merge, markers = markers,
                               clin_vars = clin_vars, percent_threshold = percent_threshold){
   #Maybe provide an error for multiple columns
   cells <- summary_clinical_merge %>% select(any_of(paste(markers)))
+  assign("percent_threshold", percent_threshold, envir = .GlobalEnv)
 
   above <- function(x, percent_threshold){ifelse(x > percent_threshold,
                                                  paste0('Greater than ', percent_threshold, '%'),
                                                  paste0('Less than ', percent_threshold, '%'))}
 
-  table <- cells %>% mutate_all( ~ above(x = ., percent_threshold = percent_threshold)) %>%
+  table <- cells %>% mutate_all( ~ above(x = ., percent_threshold = as.numeric(percent_threshold))) %>%
     bind_cols(.,summary_clinical_merge %>% select(clin_vars)) %>%
     group_by(.[[paste(markers)]],.[[clin_vars]]) %>%
     summarize(n = n()) %>%
@@ -24,19 +21,3 @@ contingency_table <- function(summary_clinical_merge, markers = markers,
   colnames(table)[1] = markers
   return(table)
 }
-
-
-#Testing
-# summary_clinical_merge = merge(clinical,summary,by.x = 'subID', by.y = 'samples')
-# markers = c('FOXP3 (Opal 620) Positive Cells','CD3 (Opal 570) Positive Cells','CD8 (Opal 520) Positive Cells')
-# clin_vars = c('race','hiv_status')
-# percent_threshold = 1
-# grid = expand.grid(markers,clin_vars)
-# 
-# for(i in 1:nrow(grid)){
-#  print(contingency_table(summary_clinical_merge, markers = grid[i,1],
-#                          clin_vars = grid[i,2], percent_threshold = percent_threshold))
-# }
-# 
-
-
