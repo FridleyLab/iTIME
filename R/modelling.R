31b1031f1edb43fbcb5b0ac156c4d93dc5ffc52f
Wilson <4468179@mlc02w42kphv2q.hlm.ad.moffitt.usf.edu>
2021-06-10 17:13:15 -0400

    Final modelling without repeated meaures

--- R/modelling.R
+++ R/modelling.R
@@ -8,44 +8,52 @@ markers = summary_data_merged %>% select(grep('Cells' ,colnames(.))) %>%
 
 a = 2
 markers = markers[a]
-Percent = paste('%',markers)
 Total = 'Total Cells'
 clin_vars = 'status'
+mods = models(summary_data_merged = summary_data_merged, markers = markers,
+       Total = Total, clin_vars, 'B')
 
-models = function(summary_data_merged = Total, markers = markers, Percent = Percent,
-                  Total = Total, clin_vars){
+
+models = function(summary_data_merged, markers,
+                  Total, clin_vars, reference){
 out = list()
-tmp = summary_data_merged %>% select(Total, markers, Percent, clin_vars)
+Percent = paste('%', markers)
+levels = unique(summary_data_merged[[clin_vars]])
+tmp = summary_data_merged %>% select(Total, markers, clin_vars) %>%
+  mutate(clin_vars = factor(.[[clin_vars]], 
+                                 levels = c(reference,
+                                            levels[levels != reference]
+                                            )))
 
-model_fit_poisson = try(glm(tmp[[markers]] ~ tmp[[clin_vars]], 
+model_fit_poisson = try(glm(tmp[[markers]] ~ tmp$clin_vars, 
                          family = poisson(link = 'log')), silent = TRUE)
 aov_poisson = coefficients(summary(model_fit_poisson))
 AIC_poisson = AIC(model_fit_poisson)
 
-model_fit_binomial = try(glm(cbind(tmp[[markers]], tmp[[Total]] - tmp[[markers]])~ tmp[[clin_vars]],
+model_fit_binomial = try(glm(cbind(tmp[[markers]], tmp[[Total]] - tmp[[markers]])~ tmp$clin_vars,
                       family = "binomial"), silent = TRUE)
 aov_binomial = coefficients(summary(model_fit_binomial))
 AIC_binomial = AIC(model_fit_binomial)
 
-model_fit_negbinom = try(vglm(tmp[[markers]] ~ tmp[[clin_vars]], negbinomial), silent = TRUE)
+model_fit_negbinom = try(vglm(tmp[[markers]] ~ tmp$clin_vars, negbinomial), silent = TRUE)
 aov_negbinom = coefficients(summary(model_fit_negbinom))
 AIC_negbinom = AIC(model_fit_negbinom)
   
-model_fit_zibinomial <- try(vglm(cbind(tmp[[markers]], tmp[[Total]] - tmp[[markers]]) ~ tmp[[clin_vars]], 
+model_fit_zibinomial <- try(vglm(cbind(tmp[[markers]], tmp[[Total]] - tmp[[markers]]) ~ tmp$clin_vars, 
                         zibinomial), silent = TRUE)
 aov_zibinomial = coefficients(summary(model_fit_zibinomial))
 AIC_zibinomial = AIC(model_fit_zibinomial)
 
-model_fit_zipoisson = try(vglm(tmp[[markers]] ~ tmp[[clin_vars]], zipoisson), silent = TRUE)
+model_fit_zipoisson = try(vglm(tmp[[markers]] ~ tmp$clin_vars, zipoisson), silent = TRUE)
 aov_zipoisson = coefficients(summary(model_fit_zipoisson))
 AIC_zipoisson = AIC(model_fit_zipoisson)
 
 
-model_fit_zinegbinomial = try(vglm(tmp[[markers]] ~ tmp[[clin_vars]], zinegbinomial, data = tmp), silent = TRUE)
+model_fit_zinegbinomial = try(vglm(tmp[[markers]] ~ tmp$clin_vars, zinegbinomial, data = tmp), silent = TRUE)
 aov_zinegbinomial = coefficients(summary(model_fit_zinegbinomial))
 AIC_zinegbinomial = AIC(model_fit_zinegbinomial)
 
-model_fit_bb = try(VGAM::vglm(cbind(tmp[[markers]], tmp[[Total]] - tmp[[markers]]) ~ tmp[[clin_vars]], 
+model_fit_bb = try(VGAM::vglm(cbind(tmp[[markers]], tmp[[Total]] - tmp[[markers]]) ~ tmp$clin_vars, 
                            betabinomial(zero = 2), data = tmp), silent = TRUE)
 aov_bb = coefficients(summary(model_fit_bb))
 AIC_bb = AIC(model_fit_bb)
@@ -57,5 +65,14 @@ out$aic = data.frame(Distribution = c('Poisson', 'Negative Binomial', 'Zero Infl
                      Family = c(rep('Poisson', 3),"" , rep('Binomial', 3)), 
                      check.names = FALSE)
 
+out$models = list('Beta Binomial' = model_fit_bb, 'Binomial' = model_fit_binomial, 
+                  'Negative Binomial' = model_fit_negbinom,
+                  'Poisson' = model_fit_poisson, 
+                  'Zero Inflated Binomial' = model_fit_zibinomial, 
+                  'Zero Inflated Poisson' = model_fit_zipoisson)
+
 return(out)
 } 
+
+
+
