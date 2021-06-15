865aa255fcb6b677ac1a766901fe70ebc873e2fb
ACSoupir <alex.soupir@jacks.sdstate.edu>
2021-06-15 10:46:01 -0400

    added try error checking in modeling; added aic table and drop downs

--- R/modelling.R
+++ R/modelling.R
@@ -1,21 +1,32 @@
-library(VGAM)
-summary_data = read.csv("example_data/deidentified_summary.csv", check.names = FALSE)
-clinical_data = read.csv("example_data/deidentified_clinical.csv", check.names = FALSE)
-summary_data_merged = merge(clinical_data, summary_data)
+# library(VGAM)
+# summary_data = read.csv("example_data/deidentified_summary.csv", check.names = FALSE)
+# clinical_data = read.csv("example_data/deidentified_clinical.csv", check.names = FALSE)
+# summary_data_merged = merge(clinical_data, summary_data)
+# 
+# markers = summary_data_merged %>% select(grep('Cells' ,colnames(.))) %>%
+#   select(!grep('\\%',colnames(.))) %>% colnames()
+# 
+# a = 2
+# markers = markers[a]
+# Total = 'Total Cells'
+# clin_vars = 'status'
+# mods = models(summary_data_merged = summary_data_merged, markers = markers,
+#        Total = Total, clin_vars, 'B')
 
-markers = summary_data_merged %>% select(grep('Cells' ,colnames(.))) %>%
-  select(!grep('\\%',colnames(.))) %>% colnames()
+#variables used
+#summary_data_merged()
+#input$picked_marker
+#input$picked_total_cells
+#input$picked_clinical
+#input$picked_modeling_reference
 
-a = 2
-markers = markers[a]
-Total = 'Total Cells'
-clin_vars = 'status'
-mods = models(summary_data_merged = summary_data_merged, markers = markers,
-       Total = Total, clin_vars, 'B')
-
+#chris wilson
+#alex edited - fixed try for zero-inflation error models (try-catch maybe better)
+#              added try-catch for all other models too
 
 models = function(summary_data_merged, markers,
                   Total, clin_vars, reference){
+markers = substr(markers, 9, nchar(markers))
 out = list()
 Percent = paste('%', markers)
 levels = unique(summary_data_merged[[clin_vars]])
@@ -27,36 +38,73 @@ tmp = summary_data_merged %>% select(Total, markers, clin_vars) %>%
 
 model_fit_poisson = try(glm(tmp[[markers]] ~ tmp$clin_vars, 
                          family = poisson(link = 'log')), silent = TRUE)
-aov_poisson = coefficients(summary(model_fit_poisson))
-AIC_poisson = AIC(model_fit_poisson)
+if(class(model_fit_poisson) == "try-error"){
+  model_fit_poisson = NULL
+  AIC_poisson = NULL
+} else {
+  aov_poisson = coefficients(summary(model_fit_poisson))
+  AIC_poisson = AIC(model_fit_poisson)
+}
 
 model_fit_binomial = try(glm(cbind(tmp[[markers]], tmp[[Total]] - tmp[[markers]])~ tmp$clin_vars,
                       family = "binomial"), silent = TRUE)
-aov_binomial = coefficients(summary(model_fit_binomial))
-AIC_binomial = AIC(model_fit_binomial)
+if(class(model_fit_binomial) == "try-error"){
+  model_fit_binomial = NULL
+  AIC_binomial = NULL
+} else {
+  aov_binomial = coefficients(summary(model_fit_binomial))
+  AIC_binomial = AIC(model_fit_binomial)
+}
 
 model_fit_negbinom = try(vglm(tmp[[markers]] ~ tmp$clin_vars, negbinomial), silent = TRUE)
-aov_negbinom = coefficients(summary(model_fit_negbinom))
-AIC_negbinom = AIC(model_fit_negbinom)
+if(class(model_fit_negbinom) == "try-error"){
+  model_fit_negbinom = NULL
+  AIC_negbinom = NULL
+} else {
+  aov_negbinom = coefficients(summary(model_fit_negbinom))
+  AIC_negbinom = AIC(model_fit_negbinom)
+}
   
 model_fit_zibinomial <- try(vglm(cbind(tmp[[markers]], tmp[[Total]] - tmp[[markers]]) ~ tmp$clin_vars, 
                         zibinomial), silent = TRUE)
-aov_zibinomial = coefficients(summary(model_fit_zibinomial))
-AIC_zibinomial = AIC(model_fit_zibinomial)
+if(class(model_fit_zibinomial) == "try-error"){
+  model_fit_zibinomial = NULL
+  AIC_zibinomial = NULL
+} else {
+  aov_zibinomial = coefficients(summary(model_fit_zibinomial))
+  AIC_zibinomial = AIC(model_fit_zibinomial)
+}
 
 model_fit_zipoisson = try(vglm(tmp[[markers]] ~ tmp$clin_vars, zipoisson), silent = TRUE)
-aov_zipoisson = coefficients(summary(model_fit_zipoisson))
-AIC_zipoisson = AIC(model_fit_zipoisson)
+if(class(model_fit_zipoisson) == "try-error"){
+  model_fit_zipoisson = NULL
+  AIC_zipoisson = NULL
+} else {
+  aov_zipoisson = coefficients(summary(model_fit_zipoisson))
+  AIC_zipoisson = AIC(model_fit_zipoisson)
+}
 
 
 model_fit_zinegbinomial = try(vglm(tmp[[markers]] ~ tmp$clin_vars, zinegbinomial, data = tmp), silent = TRUE)
-aov_zinegbinomial = coefficients(summary(model_fit_zinegbinomial))
-AIC_zinegbinomial = AIC(model_fit_zinegbinomial)
+if(class(model_fit_zinegbinomial) == "try-error"){
+  model_fit_zinegbinomial = NULL
+  AIC_zinegbinomial = NULL
+} else {
+  aov_zinegbinomial = coefficients(summary(model_fit_zinegbinomial))
+  AIC_zinegbinomial = AIC(model_fit_zinegbinomial)
+}
+
 
 model_fit_bb = try(VGAM::vglm(cbind(tmp[[markers]], tmp[[Total]] - tmp[[markers]]) ~ tmp$clin_vars, 
                            betabinomial(zero = 2), data = tmp), silent = TRUE)
-aov_bb = coefficients(summary(model_fit_bb))
-AIC_bb = AIC(model_fit_bb)
+if(class(model_fit_bb) == "try-error"){
+  model_fit_bb = NULL
+  AIC_bb = NULL
+} else {
+  aov_bb = coefficients(summary(model_fit_bb))
+  AIC_bb = AIC(model_fit_bb)
+}
+
 
 out$aic = data.frame(Distribution = c('Poisson', 'Negative Binomial', 'Zero Inflated Poisson', "",
                                       'Binomial', 'Beta Binomial', 'Zero Inflated Binomial'),
