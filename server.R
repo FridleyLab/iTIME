d42266ac8d70734f513179c980171e093ebee006
ACSoupir <alex.soupir@jacks.sdstate.edu>
2022-03-09 16:13:13 -0500

    test

--- server.R
+++ server.R
@@ -4,9 +4,9 @@
 # Along with clinical data, brief summary statistics are presented.
 #
 # Dev team in ui.R
-#clinical_data = fread("example_data/deidentified_clinical.csv", check.names = FALSE, data.table = FALSE)
-#summary_data = fread("example_data/deidentified_summary.csv", check.names = FALSE, data.table = FALSE)
-#summary_data_merged = merge(clinical_data, summary_data, by = "deidentified_id")
+# clinical_data = fread("example_data/deidentified_clinical.csv", check.names = FALSE, data.table = FALSE)
+# summary_data = fread("example_data/deidentified_summary.csv", check.names = FALSE, data.table = FALSE)
+# summary_data_merged = merge(clinical_data, summary_data, by = "deidentified_id")
 
 shinyServer(function(input, output) {
     
